59d2ee714e36fe6bc6a7cac9646cfb58867bcbc3
Saurabh Rana <saurabhrana091@gmail.com>
2022-07-19 12:22:13 +0530

    Added Collection View Cell

---
+++ Mastek_Test/Mastek_Test/CustomCell/MainCollectionViewCell/MainCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  MainCollectionViewCell.swift
+//  Mastek_Test
+//
+//  Created by Saurabh Rana on 19/07/22.
+//
+
+import UIKit
+
+class MainCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
